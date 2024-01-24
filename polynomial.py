1ccdc268d6a0303a93d897a54d8e5353771b9847
Jingbo <jw63472@bu.edu>
2024-01-24 18:53:59 -0500

    cs 506 exercise part c

--- polynomial.py
+++ polynomial.py
@@ -5,12 +5,18 @@ class X:
     def __repr__(self):
         return "X"
 
+    def evaluate(self, value):
+      return value
+
 class Int:
     def __init__(self, i):
         self.i = i
 
     def __repr__(self):
         return str(self.i)
+
+    def evaluate(self, value):
+      return self.i
 
 class Add:
     def __init__(self, p1, p2):
@@ -19,6 +25,9 @@ class Add:
 
     def __repr__(self):
         return repr(self.p1) + " + " + repr(self.p2)
+
+    def evaluate(self, value):
+      return self.p1.evaluate(value) + self.p2.evaluate(value)
 
 class Sub:
     def __init__(self, p1, p2):
@@ -38,6 +47,9 @@ class Sub:
 
         return p1_repr + " - " + p2_repr
 
+    def evaluate(self, value):
+      return self.p1.evaluate(value) - self.p2.evaluate(value)
+
 class Mul:
     def __init__(self, p1, p2):
         self.p1 = p1
@@ -51,6 +63,9 @@ class Mul:
         if isinstance(self.p2, Add):
             return repr(self.p1) + " * ( " + repr(self.p2) + " )"
         return repr(self.p1) + " * " + repr(self.p2)
+
+    def evaluate(self, value):
+      return self.p1.evaluate(value) * self.p2.evaluate(value)
 
 class Div:
   def __init__(self, p1, p2):
@@ -70,6 +85,11 @@ class Div:
 
       return p1_repr + " / " + p2_repr
 
+  def evaluate(self, value):
+    denominator = self.p2.evaluate(value)
+    if denominator == 0:
+      raise ValueError("Division by zero")
+    return self.p1.evaluate(value) / self.p2.evaluate(value)
 
 
 poly = Add( Add( Int(4), Int(3)), Add( X(), Mul( Int(1), Add( Mul(X(), X()), Int(1)))))
@@ -77,3 +97,6 @@ print(poly)
 
 test_poly = Div(Sub(X(), Int(2)), Mul(Int(3), X())) #Output: 4 + 3 + X + 1 * ( X * X + 1 )
 print(test_poly)  # Output: ((X - 2) / (3 * X))
+
+poly = Add( Add( Int(4), Int(3)), Add( X(), Mul( Int(1), Add( Mul(X(), X()), Int(1)))))
+print(poly.evaluate(-1))  # Output: 8
